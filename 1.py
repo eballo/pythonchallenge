9256ceb927db2c5bc2f36ef4e2ee921759268f0e
eballo <enric.ballo@gmail.com>
2020-02-14 14:55:46 +0100

    add 2.py + Readme

--- 1.py
+++ 1.py
@@ -11,8 +11,8 @@
 
 message = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-alphabet            = "abcdefghijklmnopqrstuvwxyz "
-alphabet_plus_three = "cdefghijklmnopqrstuvwxyzab "
+alphabet            = "abcdefghijklmnopqrstuvwxyz ()."
+alphabet_plus_three = "cdefghijklmnopqrstuvwxyzab ()."
 
 # create a tuple with the value, and translation value
 list = list(zip(alphabet, alphabet_plus_three))
@@ -20,10 +20,11 @@ print(list)
 
 nmessage = ""
 
+# Iterate over the message and get the value from the tuple
 for i in range( len(message) ):
     for x in range(len(list)):
         if(message[i] == list[x][0]):
             nmessage = nmessage + list[x][1]
 
-print("Encrypted message: "+message)
-print("Decrypted message: "+nmessage)
+print("Encrypted message: " + message)
+print("Decrypted message: " + nmessage)
---
+++ 2.py
@@ -0,0 +1,6 @@
+# Decrypted message:
+# i hope you didnt translate it by hand. thats what computers are for. doing it
+# in by hand is inefficient and thats why this text is so long. using
+# string.maketrans() is recommended. now apply on the url.
+#
+# http://www.pythonchallenge.com/pc/def/string.maketrans.html
